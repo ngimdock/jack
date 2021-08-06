a29e12b810217bcbe84a8094fb832558733aa4f6
ngimdock <ngimdockzemfack@gmail.com>
2021-08-06 17:59:28 +0100

    gestion des effets de survols

--- js/script.js
+++ js/script.js
@@ -62,8 +62,6 @@ EffectImagePortFolio();
 const animFormItems = () => {
 	const formItems = document.querySelectorAll(".form-item");
 	const formItenAfter = document.querySelectorAll(".form-item ~ div");
-	console.log(formItems);
-	console.log(formItenAfter);
 
 	for (let j = 0; j < formItems.length; j++) {
 		let i = j;
@@ -101,3 +99,81 @@ const animFormItems = () => {
 };
 
 animFormItems();
+
+
+// cette fonction me permet d'afficher les elements avec animation lors du scroll
+
+const displayElementWhentScrolling = () => {
+
+	let ratio = 0.3;
+	let options1 = { //options d'observation
+	  root: null,
+	  rootMargin: '0px',
+	  threshold: ratio
+	}
+
+	const animateAnyQuesyionBox = () => {
+
+		let timer1 = setTimeout(() => {
+			haveAnyQuestion.id = "revel-right-anim";
+			clearTimeout(timer1)
+		}, 80);
+
+		let timer2 = setTimeout(() => {
+						console.log('dans le timiout')
+						haveAnyQuestion.classList.remove("revel-visible-right");
+						haveAnyQuestion.id = ""
+						clearTimeout(timer2);
+					}, 5000);
+	};
+
+	const displayOtherElement = (entries, observer1) => {
+		entries.forEach(entry => {
+			if(entry.intersectionRatio >= ratio){
+				if(entry.target.classList.contains("c-right")){
+					haveAnyQuestion.classList.add("revel-visible-right");
+					animateAnyQuesyionBox();
+				}else{
+					entry.target.classList.add("revel-visible");
+				}
+			}else{
+				if(entry.target.classList.contains("c-right")){
+					haveAnyQuestion.classList.remove("revel-visible-right");
+					haveAnyQuestion.id = ""
+				}
+			}
+		})
+	};
+
+	let observer1 = new IntersectionObserver(displayOtherElement, options1); // instenciation de l'observateur
+
+	const otherElements = document.querySelectorAll(".revel-bottom, .c-right");
+	const haveAnyQuestion = document.querySelector(".revel-right");
+
+	console.log(otherElements);
+
+	console.log(haveAnyQuestion);
+
+	otherElements.forEach(elt => {
+		observer1.observe(elt); // observation de tous les elements selectionne
+	})
+}
+
+displayElementWhentScrolling();
+
+// cette fonction permet d'animer la partie presentation de la page
+
+const animateWebsiteHead = () => {
+	const awaardHonor = document.querySelector(".awaard-honor");
+	const jobAndContactMe = document.querySelectorAll(".head-elt");
+
+	awaardHonor.classList.add("awaardHonor-anim");
+	[...jobAndContactMe].forEach(elt => {
+		elt.classList.add("head-anim");
+	})
+
+};
+
+window.onload = () => {
+	animateWebsiteHead();
+};
